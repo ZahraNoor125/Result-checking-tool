8f8c140fbac133ed86201b4b483734d834ecbb86
Zahra Noor <159945846+ZahraNoor125@users.noreply.github.com>
2024-07-13 08:54:59 -0700

    created random result checking tool

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answer = await inquirer.prompt([
+  {
+    name: "username",
+    type: "input",
+    message: "Enter your Name",
+  },
+  { name: "userFatherName", type:
+    "input", message:
+    "Enter your Father Name" },
+
+  { name: "Roll no ", 
+    type: "number",
+    message: "Enter your Roll no" },
+]);
+
+console.log(chalk.red.bold(Math.floor(Math.random() * 48 + 52), "%"));
